64ba3180904298b6cc53cb2acf0ac3ac26b72b49
Nicolas Toro <61847756+nt-games-ytb@users.noreply.github.com>
2022-08-02 22:19:36 +0200

    Update Form1.Designer.cs

--- MamiesModGeckoTest/Form1.Designer.cs
+++ MamiesModGeckoTest/Form1.Designer.cs
@@ -39,6 +39,7 @@ namespace MamiesModGeckoTest
             // 
             this.ipText.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.ipText.Location = new System.Drawing.Point(13, 13);
+            this.ipText.MaxLength = 16;
             this.ipText.Name = "ipText";
             this.ipText.Size = new System.Drawing.Size(100, 22);
             this.ipText.TabIndex = 0;
