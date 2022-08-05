eb2c2a8e98b006211b6cccfa96dcc50dbfa18283
Nicolas Toro <61847756+nt-games-ytb@users.noreply.github.com>
2022-08-06 00:56:21 +0200

    Add files via upload

---
+++ MamiesModGeckoTest/21 11 06/MamiesModGeckoTest/Form1.Designer.cs
@@ -0,0 +1,111 @@
+﻿
+namespace MamiesModGeckoTest
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Variable nécessaire au concepteur.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Nettoyage des ressources utilisées.
+        /// </summary>
+        /// <param name="disposing">true si les ressources managées doivent être supprimées ; sinon, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Code généré par le Concepteur Windows Form
+
+        /// <summary>
+        /// Méthode requise pour la prise en charge du concepteur - ne modifiez pas
+        /// le contenu de cette méthode avec l'éditeur de code.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.ipText = new System.Windows.Forms.TextBox();
+            this.connect = new System.Windows.Forms.Button();
+            this.disconnect = new System.Windows.Forms.Button();
+            this.xray = new System.Windows.Forms.CheckBox();
+            this.SuspendLayout();
+            // 
+            // ipText
+            // 
+            this.ipText.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.ipText.Location = new System.Drawing.Point(13, 13);
+            this.ipText.Name = "ipText";
+            this.ipText.Size = new System.Drawing.Size(100, 22);
+            this.ipText.TabIndex = 0;
+            this.ipText.Text = "192.168.1.48";
+            this.ipText.TextChanged += new System.EventHandler(this.ipText_TextChanged);
+            // 
+            // connect
+            // 
+            this.connect.Enabled = false;
+            this.connect.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.connect.Location = new System.Drawing.Point(119, 12);
+            this.connect.Name = "connect";
+            this.connect.Size = new System.Drawing.Size(125, 23);
+            this.connect.TabIndex = 1;
+            this.connect.Text = "Se connecter";
+            this.connect.UseVisualStyleBackColor = true;
+            this.connect.Click += new System.EventHandler(this.connect_Click);
+            // 
+            // disconnect
+            // 
+            this.disconnect.Enabled = false;
+            this.disconnect.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.disconnect.Location = new System.Drawing.Point(250, 12);
+            this.disconnect.Name = "disconnect";
+            this.disconnect.Size = new System.Drawing.Size(125, 23);
+            this.disconnect.TabIndex = 2;
+            this.disconnect.Text = "Se déconnecter";
+            this.disconnect.UseVisualStyleBackColor = true;
+            this.disconnect.Click += new System.EventHandler(this.disconnect_Click);
+            // 
+            // xray
+            // 
+            this.xray.AutoSize = true;
+            this.xray.Location = new System.Drawing.Point(13, 41);
+            this.xray.Name = "xray";
+            this.xray.Size = new System.Drawing.Size(45, 17);
+            this.xray.TabIndex = 3;
+            this.xray.Text = "xray";
+            this.xray.UseVisualStyleBackColor = true;
+            this.xray.CheckedChanged += new System.EventHandler(this.xray_CheckedChanged);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(390, 144);
+            this.Controls.Add(this.xray);
+            this.Controls.Add(this.disconnect);
+            this.Controls.Add(this.connect);
+            this.Controls.Add(this.ipText);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedDialog;
+            this.MaximizeBox = false;
+            this.Name = "Form1";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "MamiesModGecko Test";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox ipText;
+        private System.Windows.Forms.Button connect;
+        private System.Windows.Forms.Button disconnect;
+        private System.Windows.Forms.CheckBox xray;
+    }
+}
+
---
+++ MamiesModGeckoTest/22 08 06/MamiesModGeckoTest/MamiesModGeckoTestForm.cs
@@ -0,0 +1,186 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Globalization;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using MamiesModGecko;
+using MamiesModGeckoTest;
+
+namespace MamiesModGeckoTest
+{
+    public partial class MamiesModGeckoTestForm : Form
+    {
+        private TCPConn Connection;
+        private TCPGecko MamiesModGecko;
+        private BeforeConnect BeforeConnect = new BeforeConnect();
+        public string nintendoNetwork;
+
+        #region Code Handler
+        public List<uint> codesList = new List<uint> {};
+        
+        bool xrayCode = false;
+        #endregion
+
+        public MamiesModGeckoTestForm()
+        {
+            InitializeComponent();
+        }
+
+        private void MamiesModGeckoTestForm_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void ipText_TextChanged(object sender, EventArgs e)
+        {
+            try
+            {
+                if (BeforeConnect.ValidateIPv4(ipText.Text) == true)
+                {
+                    connect.Enabled = true;
+                }
+                else
+                {
+                    connect.Enabled = false;
+                }
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.ToString());
+            }
+        }
+
+        private void connect_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                MamiesModGecko = new TCPGecko(ipText.Text);
+                MamiesModGecko.simplyConnect();
+                GetNintendoNetwork();
+                MessageBox.Show("Welcome " + nintendoNetwork + ",\n you are now connected on MamiesModGecko Test !", "MamiesModGecko Test");
+
+                ipText.Enabled = false;
+                connect.Enabled = false;
+                disconnect.Enabled = true;
+                xrayPoke.Enabled = true;
+                xrayCodeHandler.Enabled = true;
+            }
+            catch (IOException ex)
+            {
+                MessageBox.Show(ex.Message, "MamiesModGecko Test");
+            }
+            catch (System.Net.Sockets.SocketException)
+            {
+                MessageBox.Show("Error: your ip is not the right one or you are not connected to the internet !", "MamiesModGecko Test");
+            }
+            catch
+            {
+                MessageBox.Show("An unknown error has occurred !", "MamiesModGecko Test");
+            }
+        }
+
+        private void disconnect_Click(object sender, EventArgs e)
+        {
+            MamiesModGecko.Disconnect();
+
+            ipText.Enabled = true;
+            connect.Enabled = true;
+            disconnect.Enabled = false;
+            xrayPoke.Enabled = false;
+            xrayCodeHandler.Enabled = false;
+        }
+
+        #region Get Nintendo Network
+        private void GetNintendoNetwork() //only for Minecraft Wii U
+        {
+            string text1 = MamiesModGecko.peekStringUTF16(MamiesModGecko.peek(MamiesModGecko.peek(0x10AD1C58) + 0x4C));
+            string text2 = MamiesModGecko.peekStringUTF16(MamiesModGecko.peek(MamiesModGecko.peek(0x10AD1C58) + 0x50));
+            string text3 = MamiesModGecko.peekStringUTF16(MamiesModGecko.peek(MamiesModGecko.peek(0x10AD1C58) + 0x54));
+            string text4 = MamiesModGecko.peekStringUTF16(MamiesModGecko.peek(MamiesModGecko.peek(0x10AD1C58) + 0x58));
+            string text5 = MamiesModGecko.peekStringUTF16(MamiesModGecko.peek(MamiesModGecko.peek(0x10AD1C58) + 0x5C));
+            string text6 = MamiesModGecko.peekStringUTF16(MamiesModGecko.peek(MamiesModGecko.peek(0x10AD1C58) + 0x60));
+            string text7 = MamiesModGecko.peekStringUTF16(MamiesModGecko.peek(MamiesModGecko.peek(0x10AD1C58) + 0x64));
+            string text8 = MamiesModGecko.peekStringUTF16(MamiesModGecko.peek(MamiesModGecko.peek(0x10AD1C58) + 0x68));
+            string text9 = MamiesModGecko.peekStringUTF16(MamiesModGecko.peek(MamiesModGecko.peek(0x10AD1C58) + 0x68));
+            nintendoNetwork = text1 + text2 + text3 + text4 + text5 + text6 + text7 + text8 + text9;
+        }
+        #endregion
+
+        private void updateCodesListForEnableCodes() //for enable all codes
+        {
+            codesList.Clear();
+
+            if (xrayCode == true)
+            {
+                MamiesModGecko.addCodeToCodesList(codesList, "00020000 108F73E0\n40000000 00000000\nD0000000 DEADCAFE");
+                // "\n" is a line break | You can also put spaces or put commas
+
+                /*
+                or : 
+                codesList.Add(0x00020000);
+                codesList.Add(0x108F73E0);
+                codesList.Add(0x40000000);
+                codesList.Add(0x00000000);
+                */
+            }
+
+            MamiesModGecko.enableCodes(codesList);
+        }
+
+        private void updateCodesListForDisableCodes() //for disable codes that need to reset to default
+        {
+            codesList.Clear();
+
+            if (xrayCode == false)
+            {
+                MamiesModGecko.addCodeToCodesList(codesList, "00020000 108F73E0\n3F000000 00000000\nD0000000 DEADCAFE");
+                // "\n" is a line break | You can also put spaces or put commas
+
+                /*
+                or : 
+                codesList.Add(0x00020000);
+                codesList.Add(0x108F73E0);
+                codesList.Add(0x3F000000);
+                codesList.Add(0x00000000);
+                */
+            }
+
+            MamiesModGecko.enableCodes(codesList);
+            Thread.Sleep(750);
+            MamiesModGecko.disableCodes();
+        }
+
+        private void xrayPoke_CheckedChanged(object sender, EventArgs e)
+        {
+            if (xrayPoke.Checked)
+            {
+                MamiesModGecko.poke32(0x108F73E0, 0x40000000);
+            }
+            else
+            {
+                MamiesModGecko.poke32(0x108F73E0, 0x3F000000);
+            }
+        }
+
+        private void xrayCodeHandler_CheckedChanged(object sender, EventArgs e)
+        {
+            if (xrayCodeHandler.Checked)
+            {
+                xrayCode = true;
+                updateCodesListForEnableCodes();
+            }
+            else
+            {
+                xrayCode = false;
+                updateCodesListForDisableCodes();
+            }
+        }
+    }
+}
